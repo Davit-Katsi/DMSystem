69ac2e97ccd628232cbcde0b0fa1fb12736aa02d
Davit-Katsi <davitkaikatsishvili@gmail.com>
2025-02-19 02:21:23 +0400

    Fix: Added production support for DATABASE_URL in Sequelize

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,15 +1,30 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config');
 
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  {
-    host: config.development.host,
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     logging: false,
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,
+    {
+      host: config.development.host,
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
